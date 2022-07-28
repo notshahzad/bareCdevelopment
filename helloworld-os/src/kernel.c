b4fc985f9c7710cb78f4b4a23bb7b677038ae954
notshahzad <shahzadakhtar182005@gmail.com>
2022-07-28 20:06:59 +0530

    segfaulty printf implementation

--- helloworld-os/src/kernel.c
+++ helloworld-os/src/kernel.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <stdarg.h>
 uint16_t* termbuffer = (uint16_t*) 0x0B8000;
 int termrow = 25;
 int termcolumn = 80;
@@ -13,20 +14,58 @@ int strlen(const char* str){
   return len;
 }
 void putchar(const char chr){
-  termbuffer[(currentrow * termcolumn) + currentcolumn] = (uint16_t) chr | 0x0F00;
-
-}
-void term_print(const char *string){
-  int len = strlen(string);
-  for (int i = 0; i < len; i++){
-    if (string[i] == '\n'){
+    if (chr == '\n'){
       currentcolumn = 0;
       currentrow += 1;
-      continue;
+      return;
     }
-    putchar(string[i]);
-    currentcolumn += 1;
+  termbuffer[(currentrow * termcolumn) + currentcolumn] = (uint16_t) chr | 0x0F00;
+  currentcolumn ++;
+
+}
+void writeString(const char* string);
+void k_print(const char* format, ...){
+  int len = strlen(format);
+  va_list args;
+  va_start(args, format);
+  for (int i = 0; i < len; i++){
+    if (format[i] == '%'){
+      int var;
+      switch (format[i+1]) {
+        case 'd':
+          var = va_arg(args,int);
+          if (var < 0) putchar('-');
+          void dec(int vr){
+            int temp = vr % 10;
+            vr = (int) vr / 10;
+            if (vr > 0)
+              dec(vr);
+            putchar(temp + 48);
+          }
+          dec(var);
+          i+=1;
+          break;
+        case 's':
+          writeString((const char*)va_arg(args, char *));
+          i+=1;
+          break;
+        case 'c':
+          putchar((char)va_arg(args, int));
+          i+=1;
+          break;
+        default:
+          break;
+      }
+    }else{
+      putchar(format[i]);
+    }
   }
+}
+
+void writeString(const char *string){
+  int len = strlen(string);
+  for (int i = 0; i < len; i++)
+    putchar(string[i]);
 }
 void term_init(){
   for (int i = 0; i < termrow; i++){
@@ -38,6 +77,5 @@ void term_init(){
 }
 void kernel_main(void){
   term_init();
- term_print("hello,world!\n"); 
- term_print("okeh");
+  k_print("%d %c %s",123435,'c',"asdjaks");
 }
